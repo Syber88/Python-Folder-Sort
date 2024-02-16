f4a0ad3be39b4113d19608f390d6fa133cbea00f
smagwaza023 <smagwaza023@student.wethinkcode.co.za>
2024-02-16 17:24:48 +0200

    EOD push

--- folder_sort.py
+++ folder_sort.py
@@ -3,36 +3,40 @@ import sys
 import shutil #allows us to delete a directory that is not empty
 from pprint import pprint
 
-pwd = os.getcwd()
-print(pwd[:6])
-os.chdir("/home/wethinkcode/Downloads") #make this path dynamic for future use 
+def get_username():
+    pwd = os.getcwd() #does not matter which directory you are in, but what if you're in the home directory. sort that out later 
+    dir_split = pwd.split("/")
+    user_profile_index = dir_split.index("home")
+    return dir_split[user_profile_index+1]
+
+
+profile = get_username()
+
+os.chdir(f"/home/{profile}/Downloads") #make this path dynamic for future use 
 """now we want to analyse the different extentions of differnt files and check whether somesthing is a file or a folder"""
 dir_list = os.listdir()
 
+
 def create_extension_directory(extension):
     if os.path.exists(extension):
+        print(f"Direcory {extension} already exists" )
         return
     else:
         os.mkdir(f"{extension}")
-        
-def get_username():
-    dir_split = pwd.split("/")
-    user_profile_index = dir_split.index("home")
-    return dir_split[user_profile_index]
-     
-    # for dir in dir_split:
-    #     if dir == "home":
-    #         home_index = 
+
 
 """isolate the current working direcrory using getcwd"""
-for files in dir_list:
+"""break down the code below into functions"""
+"""get a way to breakdown the code into separate functions"""
+"""function names will be, extensions identifier, extension move"""
+for files in dir_list: 
     if os.path.isfile(files):
         dotfind = files.rfind(".")
         if files[dotfind+1::] == "zip":
             create_extension_directory("zip") #having a function to create a directory
             #is unnecessary since shutil will automatically make one if it does not already exist
-            source = f"/home/wethinkcode/Downloads/"
-            destination_dir = f"/home/wethinkcode/Downloads/zip/"
+            source = f"/home/{profile}/Downloads/"
+            destination_dir = f"/home/{profile}/Downloads/zip/"
             if os.path.exists(destination_dir + f"{files}"):
                 rename = files[:dotfind] + "_new"
                 shutil.move(source + f"{files}", destination_dir + f"{rename}")
